18ff43736229a737ac4a2fee58caca86fe90168c
Joseph Chávez <79435044+josephc21@users.noreply.github.com>
2021-07-27 14:02:22 -0700

    mas información

---
+++ L03-OperadoresJS/OperadoresJS.js
@@ -0,0 +1,48 @@
+// OPERADOR SUMA 
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+// OPERADOR RESTA 
+z = a - b;
+console.log("El resultado de la resta: " + z);
+
+// OPERADOR DE MULTIPLICACION
+z = a * b;
+console.log("Resultado de la multiplicacion: " + z);
+
+// OPERADOR DE DIVISION 
+z = a / b ;
+console.log("Resultado de la división: " + z);
+
+// OPERADOR DE MODULO : RESIDUO DE LA DIVISION 
+z = a % b;
+console.log("Resultado de la operacion modulo: " + z);
+
+// OPERADOR DE EXPONENTE 
+z = a**b;
+console.log("El resultado del operador exponente: " + z);
+
+/** OPERADORES DE DECREMENTO E INCREMENTO **/
+
+// (INCREMENTO)
+/* PRE-INCREMENTO: (El operador ++ antes de la variable) */
+z =  ++a;
+console.log(a);
+console.log(z);
+
+/* POST-INCREMENTO: (El operador ++ despues de la variable) */
+z = b++;
+console.log(b);
+console.log(z);
+
+// (DECREMENTO)
+/* PRE-DECREMENTO */
+z = --a;
+console.log(a);
+console.log(z);
+
+/* POST-DECREMENTO */
+z = b--;
+console.log(b);
+console.log(z);
