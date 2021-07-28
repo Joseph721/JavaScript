43dae8330f65e38d54227872c3fe28706389fd30
Joseph Chávez <79435044+josephc21@users.noreply.github.com>
2021-07-27 20:16:39 -0700

    mas informacion

---
+++ L03-OperadoresJS/OperadoresAsignacionComparacionJS.js
@@ -0,0 +1,20 @@
+// OPERADORES DE ASIGNACION EN JAVASCRIPT 
+// let a = 1;
+
+// a += 3;
+// console.log(a);
+
+// a -=2;
+// console.log(a);
+
+
+// OPERADORES DE COMPARACION EN JAVASCRIPT 
+let a = 3, b =2 , c = "3";
+
+let z = a == b; //SE REVISA EL VALOR SIN IMPROTAR EL
+console.log(z);
+
+
+z = a === c; // ESTE OPERADOR REVISA SI LOS VALORES SON IGUALES PERO TAMBIEN LOS TIPOS
+// SI LOS TIPOS NO SON IGUALES RETORNA "FALSO" 
+console.log(z);
