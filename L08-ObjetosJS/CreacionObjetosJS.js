8515969bdde03b4e58e93b0a85cd14910dc07e82
Joseph Chávez <79435044+josephc21@users.noreply.github.com>
2021-08-04 13:28:55 -0700

    Más objetos

--- L08-ObjetosJS/CreacionObjetosJS.js
+++ L08-ObjetosJS/CreacionObjetosJS.js
@@ -3,5 +3,11 @@ let auto = new Object();
 auto.marca = "BMW";
 auto.color = "Rojo";
 auto.numeroPuertas = 4;
+encenderAuto: function(){
+    console.log("El auto está encendido.")
+}
+apagarAuto: function(){
+    console.log("El auto está apagado.")
+}
 
 console.log(auto.marca);
